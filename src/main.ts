a2469bddd778093fbb9137fdd0827e40936c3efc
Maiconfz <maiconfz@gmail.com>
2024-03-22 00:45:06 +0000

    Move some dependecies to vite main.ts instead of external link on html

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import appHtml from './app.html?raw';
 import './js/app.js';
 import './style.css';
